1247b651ecfaa5f478d528bfad9a19d15b3bf2be
Josslyn-Montrone <162729280+Josslyn-Montrone@users.noreply.github.com>
2024-04-17 16:52:14 +0000

    Submit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+temp = int(input("Enter the the numerical temperature alone: "))
+choice = input("Type 1 for Fahrenheight-to-Centigrade. Type 2 for Centigrade-to-Fahrenheight: ")
+if choice == "1":
+    converted_temp = (5/9) * (temp - 32)
+    print("The converted temperature is " + str(converted_temp) + " Centigrade.")
+elif choice == "2":
+    converted_temp = (9 * temp + (32 * 5))/5
+    print("The converted temperature is " + str(converted_temp) + " Fahrenheight.")
+else:
+    print("Invalid input.")
